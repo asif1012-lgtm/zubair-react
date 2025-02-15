ccc436a947f90aaf96bb8a976643e64280061162
notifactionface <39661594-notifactionface@users.noreply.replit.com>
2025-02-15 05:38:03 +0000

    Agent query: Is the new gradient (light pink to light blue to light green) displaying correctly on the left side of the page?
    Update home page background image to linear gradient.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/574cdf3d-0148-441b-9e03-14fd12ecb045/130a992e-3493-4d57-87d9-e749c99f032b.jpg

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -12,10 +12,7 @@ export default function Home() {
       <div 
         className="w-full md:w-[42%] h-[27vh] md:h-screen shrink-0"
         style={{
-          backgroundImage: 'url("https://pub-804f6dcb45594c119ba6e0d63e10c013.r2.dev/bg-img.jpg")',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat'
+          background: 'linear-gradient(130deg, rgba(249, 241, 249, 1) 0%, rgba(234, 243, 253, 1) 35%, rgba(237, 251, 242, 1) 100%)'
         }}
       />
       <div className="flex-1 flex flex-col items-center justify-center p-4 sm:p-6 md:p-8 bg-white">
